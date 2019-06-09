310b8a7f512423558a888f02ba3109de92533cfd
Justin <justleland@gmail.com>
2019-06-09 18:47:19 -0400

    Better Application program

--- src/main/java/rocks/zipcode/atm/LoginPage.java
+++ src/main/java/rocks/zipcode/atm/LoginPage.java
@@ -27,12 +27,29 @@ import rocks.zipcode.atm.bank.Account;
 
 public class LoginPage {
 
+    private String error = "";
+    private CashMachine cashMachine;
+    private Boolean login = false;
+
+    public LoginPage(CashMachine cashMachine){
+        this.cashMachine = cashMachine;
+    }
+
     //@Override
 
   //  public  void start (Stage primaryStage) {
 
-    public static Parent createContent() {
+    public Parent createContent() {
       // primaryStage.setTitle("Bank of Whatever");
+        GridPane othergrid = new GridPane();
+        othergrid.setAlignment(Pos.CENTER);
+        othergrid.setHgap(10);
+        othergrid.setVgap(10);
+        othergrid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text otherHeading = new Text("Bank Page");
+        otherHeading.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        othergrid.add(otherHeading, 0, 0, 2, 1);
 
         // positioning of box
         GridPane grid = new GridPane();
@@ -58,16 +75,20 @@ public class LoginPage {
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
 
+        Label errorMsg = new Label(error);
+        grid.add(errorMsg, 0, 3, 2, 1);
+
         //buttons
         Button btn = new Button("Login");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
-       // btn.setOnAction(e -> {
-        // should lead to user page when clicked
-          //  cashmachine.
-              //  }
+        btn.setOnAction(e -> {
+            Boolean isLoggedIn = cashMachine.userLogin(userTextField.getText(), Integer.parseInt(pwBox.getText()));
+            System.out.println("Print this");
+            if(!isLoggedIn) errorMsg.setText("Incorrect User ID or PIN.");
+                });
 
 
         //Scene scene = new Scene(grid, 300, 275);
@@ -75,6 +96,7 @@ public class LoginPage {
 
         // primaryStage.show();
 
+        if(login) return othergrid;
         return grid;
 
 
