fbe1c5bd41422df496580e2255cb1d7a72e10ba7
Abram Phillips <abramcpg@hotmail.com>
2019-06-09 20:02:21 -0400

    Added VisualEffects Class and altered CashMachine Class

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,17 +1,17 @@
 package rocks.zipcode.atm;
 
-
+import rocks.zipcode.atm.bank.Bank;
 import javafx.application.Application;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.TextArea;
+        import javafx.scene.control.TextField;
+        import javafx.scene.layout.VBox;
+        import javafx.stage.Stage;
+        import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
 
-import rocks.zipcode.atm.bank.Bank;
 /**
  * @author ZipCodeWilmington
  */
@@ -20,13 +20,27 @@ public class CashMachineApp extends Application {
     private TextField field = new TextField();
     private CashMachine cashMachine = new CashMachine(new Bank());
 
+    private static final Color lightBlue = Color.web("#8BF4FF"); //for blue text
+    private static final Color black = Color.web("#000000"); //for black text
+
     private Parent createContent() {
         VBox vbox = new VBox(10);
+
         vbox.setPrefSize(600, 600);
 
         TextArea areaInfo = new TextArea();
 
-        Button btnSubmit = new Button("Set Account ID");
+        //Declare buttons
+        Button btnSubmit = new Button("Login"); //Change to "Set Account ID"
+        Button btnWithdraw = new Button("Withdraw");
+        Button btnDeposit = new Button("Deposit");
+        Button btnExit = new Button("Exit");
+
+        //Declare background
+        FlowPane flowpane = new FlowPane();
+
+
+        //Submit
         btnSubmit.setOnAction(e -> {
             int id = Integer.parseInt(field.getText());
             cashMachine.login(id);
@@ -34,7 +48,7 @@ public class CashMachineApp extends Application {
             areaInfo.setText(cashMachine.toString());
         });
 
-        Button btnDeposit = new Button("Deposit");
+        //Deposit
         btnDeposit.setOnAction(e -> {
             int amount = Integer.parseInt(field.getText());
             cashMachine.deposit(amount);
@@ -42,7 +56,7 @@ public class CashMachineApp extends Application {
             areaInfo.setText(cashMachine.toString());
         });
 
-        Button btnWithdraw = new Button("Withdraw");
+        //Withdraw
         btnWithdraw.setOnAction(e -> {
             int amount = Integer.parseInt(field.getText());
             cashMachine.withdraw(amount);
@@ -50,15 +64,16 @@ public class CashMachineApp extends Application {
             areaInfo.setText(cashMachine.toString());
         });
 
-        Button btnExit = new Button("Exit");
+        //Exit
         btnExit.setOnAction(e -> {
             cashMachine.exit();
 
             areaInfo.setText(cashMachine.toString());
         });
 
-        FlowPane flowpane = new FlowPane();
 
+
+        flowpane.setStyle("-fx-background-color:#8BF4FF"); //Background color blue
         flowpane.getChildren().add(btnSubmit);
         flowpane.getChildren().add(btnDeposit);
         flowpane.getChildren().add(btnWithdraw);
---
+++ src/main/java/rocks/zipcode/atm/VisualEffects.java
@@ -0,0 +1,24 @@
+package rocks.zipcode.atm;
+
+import javafx.scene.paint.Color;
+
+import javafx.scene.control.*;
+
+
+public class VisualEffects {
+
+    //Text colors
+    private static final Color lightBlue = Color.web("#8BF4FF"); //for blue text
+    private static final Color black = Color.web("#000000"); //for black text
+
+    public static void turnInvisible(Labeled button){ //Turn text and button invisible
+        button.setStyle("-fx-background-color:#8BF4FF"); //turn button blue
+        button.setTextFill(lightBlue); //turn text blue
+    }
+
+    public static void turnVisible(Labeled button){ //Turn text and button invisible
+        button.setStyle("-fx-background-color:#FFFFFF"); //turn button blue
+        button.setTextFill(lightBlue); //turn text blue
+    }
+
+}
